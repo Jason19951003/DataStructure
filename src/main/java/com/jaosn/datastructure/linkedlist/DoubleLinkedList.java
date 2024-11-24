2e03de58218f6b12946ad005b6497febad2131a5
Jason19951003 <jason19951003@gmail.com>
2024-11-24 18:41:00 +0800

    新增DoubleLinkedList DoubleNode

---
+++ src/main/java/com/jaosn/datastructure/linkedlist/DoubleLinkedList.java
@@ -0,0 +1,10 @@
+package com.jaosn.datastructure.linkedlist;
+
+public class DoubleLinkedList {
+
+	private Node head;
+	
+	public DoubleLinkedList(Node head) {
+		this.head = head;
+	}
+}
---
+++ src/main/java/com/jaosn/datastructure/linkedlist/DoubleNode.java
@@ -0,0 +1,12 @@
+package com.jaosn.datastructure.linkedlist;
+
+public class DoubleNode extends Node {
+
+	private Node pre;
+	
+	public DoubleNode(int no, int value) {
+		super(no, value);
+		pre = null;
+	}
+
+}
